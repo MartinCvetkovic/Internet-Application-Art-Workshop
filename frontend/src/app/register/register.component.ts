0677298942e68efeaf70ae888f32a7bbb0fba6ad
MartinCvetkovic <martincvetkovic2000@yahoo.com>
2023-02-03 13:22:59 +0100

    Registration front finished

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.type = "participant";
   }
 
+  firstname:          string; 
+  lastname:           string;
+  username:           string;
+  password:           string;
+  repeatedPassword:   string;
+  phone:              string;
+  email:              string;
+  image:              string;
+  type:               string;
+  orgname:            string;
+  country:            string;
+  city:               string;
+  postal_code:        string;
+  address:            string;
+  adrNumber:          string;
+  reg_number:         number;
+
+  errorMessage:       string;
+
+  register() {
+    alert(this.type)
+  }
 }
