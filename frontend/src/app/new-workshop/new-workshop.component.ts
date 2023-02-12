e8505dc5c17206852870ac8e98d8e4dbc45e749a
MartinCvetkovic <martincvetkovic2000@yahoo.com>
2023-02-12 13:39:41 +0100

    New workshop front looks done

--- frontend/src/app/new-workshop/new-workshop.component.ts
+++ frontend/src/app/new-workshop/new-workshop.component.ts
@@ -10,6 +10,50 @@ export class NewWorkshopComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem('user'));
+
+    this.imageError = null;
   }
 
+  name: string;
+  date: string;
+  place: string;
+  short_description: string;
+  long_description: string;
+  main_img: string;
+  gallery_images: string[];
+
+  imageError: string;
+  successInfo: string;
+  errorMessage: string;
+
+  user: Object;
+
+  
+  fileChangeEvent(fileInput: any) {
+    this.imageError = null;
+    this.successInfo = null;
+    if (fileInput.target.files && fileInput.target.files[0]) {
+      if (!['image/png', 'image/jpg', 'image/jpeg'].includes(fileInput.target.files[0].type)) {
+        this.imageError = 'Only JPG and PNG images are allowed.';
+        return;
+      }
+
+      let reader = new FileReader();
+      reader.onload = (e: any) => {
+        let image = new Image();
+        image.src = e.target.result;
+        image.onload = rs => {
+          this.main_img = e.target.result;
+          console.log(this.main_img)
+        };
+      };
+
+      reader.readAsDataURL(fileInput.target.files[0]);
+    }
+  }
+
+  addWorkshop() {
+
+  }
 }
